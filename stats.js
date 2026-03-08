a0ce26584bc8350e3f24e852276dc9feac3053cf
norugogi <55315428+norugogi@users.noreply.github.com>
2026-03-08 17:39:23 +0900

    Refactor stats.js for better code organization
    Refactor stats.js to improve structure and readability.

--- stats.js
+++ stats.js
@@ -1,222 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(){
-initStats()
-})
-
-fetch("players.json")
-
-.then(res=>res.json())
-
-.then(data=>{
-
-let players=data.map(p=>({
-
-name:p.name,
-level:Number(p.level),
-grade:Number(p.grade),
-class:p.class,
-guild:p.guild,
-world:p.world
-
-}))
-
-
-const classMap={
-
-SolarSentinel:"태양감시자",
-AbyssRevenant:"심연추방자",
-MirageBlade:"환영검사",
-Enforcer:"집행관",
-IncenseArcher:"향사수",
-Runescribe:"주문각인사"
-
-}
-
-
-players.forEach(p=>{
-
-p.class=classMap[p.class] || p.class
-
-})
-
-
-const total=players.length
-
-
-
-function countBy(arr,key){
-
-let result={}
-
-arr.forEach(p=>{
-
-let v=p[key]
-
-if(!result[v]) result[v]=0
-
-result[v]++
-
-})
-
-return result
-
-}
-
-
-function renderChart(canvasId,labels,data){
-
-new Chart(
-
-document.getElementById(canvasId),
-
-{
-
-type:"bar",
-
-data:{
-
-labels:labels,
-
-datasets:[{
-
-label:"인원",
-
-data:data,
-
-backgroundColor:"#4e79ff"
-
-}]
-
-},
-
-options:{
-
-plugins:{
-
-legend:{display:false}
-
-},
-
-scales:{
-
-x:{ticks:{color:"white"}},
-
-y:{ticks:{color:"white"}}
-
-}
-
-}
-
-})
-
-}
-
-
-function renderTable(tableId,labels,data){
-
-let tbody=document.querySelector("#"+tableId+" tbody")
-
-tbody.innerHTML=""
-
-labels.forEach((l,i)=>{
-
-let count=data[i]
-
-let percent=((count/total)*100).toFixed(1)
-
-tbody.innerHTML+=`
-
-<tr>
-
-<td>${l}</td>
-
-<td>${count}</td>
-
-<td>${percent}%</td>
-
-</tr>
-
-`
-
-})
-
-}
-
-
-
-const levelData=countBy(players,"level")
-
-renderChart(
-
-"levelChart",
-
-Object.keys(levelData),
-
-Object.values(levelData)
-
-)
-
-renderTable(
-
-"levelTable",
-
-Object.keys(levelData),
-
-Object.values(levelData)
-
-)
-
-
-
-const gradeData=countBy(players,"grade")
-
-renderChart(
-
-"gradeChart",
-
-Object.keys(gradeData),
-
-Object.values(gradeData)
-
-)
-
-renderTable(
-
-"gradeTable",
-
-Object.keys(gradeData),
-
-Object.values(gradeData)
-
-)
-
-
-
-const classData=countBy(players,"class")
-
-renderChart(
-
-"classChart",
-
-Object.keys(classData),
-
-Object.values(classData)
-
-)
-
-renderTable(
-
-"classTable",
-
-Object.keys(classData),
-
-Object.values(classData)
-
-)
-
-
-
-})
-
 function showTab(name){
 
 document.querySelectorAll(".tab-content").forEach(el=>{
@@ -233,3 +14,96 @@ event.target.classList.add("active")
 
 }
 
+
+
+fetch("players.json")
+.then(res => res.json())
+.then(players => {
+
+function countBy(key){
+
+const map = {}
+
+players.forEach(p=>{
+const v = p[key]
+map[v] = (map[v] || 0) + 1
+})
+
+return map
+}
+
+
+
+function renderChart(id, labels, data){
+
+new Chart(
+document.getElementById(id),
+{
+type:"bar",
+data:{
+labels:labels,
+datasets:[{
+label:"인원",
+data:data
+}]
+}
+})
+
+}
+
+
+
+function renderTable(id, labels, data){
+
+const div = document.getElementById(id)
+
+let html = "<table>"
+html += "<tr><th>값</th><th>인원</th></tr>"
+
+labels.forEach((l,i)=>{
+html += "<tr><td>"+l+"</td><td>"+data[i]+"</td></tr>"
+})
+
+html += "</table>"
+
+div.innerHTML = html
+
+}
+
+
+
+const levelData = countBy("level")
+
+const levelLabels = Object.keys(levelData).sort((a,b)=>a-b)
+const levelValues = levelLabels.map(l=>levelData[l])
+
+const levelLabelsFiltered = levelLabels.filter(l=>Number(l)>=80)
+const levelValuesFiltered = levelLabelsFiltered.map(l=>levelData[l])
+
+renderChart("levelChart",levelLabelsFiltered,levelValuesFiltered)
+renderTable("levelTable",levelLabels,levelValues)
+
+
+
+const gradeData = countBy("grade")
+
+const gradeLabels = Object.keys(gradeData).sort((a,b)=>a-b)
+const gradeValues = gradeLabels.map(l=>gradeData[l])
+
+const gradeLabelsFiltered = gradeLabels.filter(l=>Number(l)>=15)
+const gradeValuesFiltered = gradeLabelsFiltered.map(l=>gradeData[l])
+
+renderChart("gradeChart",gradeLabelsFiltered,gradeValuesFiltered)
+renderTable("gradeTable",gradeLabels,gradeValues)
+
+
+
+const classData = countBy("class")
+
+const classLabels = Object.keys(classData)
+const classValues = classLabels.map(l=>classData[l])
+
+renderChart("classChart",classLabels,classValues)
+renderTable("classTable",classLabels,classValues)
+
+})
