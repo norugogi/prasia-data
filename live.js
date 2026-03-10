704c9d0e8831500aec15b8fa39d21d55655f36ff
norugogi <55315428+norugogi@users.noreply.github.com>
2026-03-10 17:41:50 +0900

    Add files via upload

---
+++ live.js
@@ -0,0 +1,30 @@
+let data=[];
+
+function update(){
+
+fetch("all_servers_ranking.json")
+.then(res=>res.json())
+.then(json=>{
+
+data=json;
+
+let count=data.filter(p=>p.grade>=25).length;
+
+let now=new Date();
+
+let time=
+String(now.getMonth()+1).padStart(2,"0")+"/"+
+String(now.getDate()).padStart(2,"0")+" "+
+String(now.getHours()).padStart(2,"0")+":"+
+String(now.getMinutes()).padStart(2,"0");
+
+document.getElementById("counter").innerText=
+`토벌 25 달성 현황 : ${count}명 (${time} 기준)`;
+
+});
+
+}
+
+update();
+
+setInterval(update,30000);
